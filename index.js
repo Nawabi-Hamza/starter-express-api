3eefb42402c1155f2deed93a999c8356791eca8a
Nawabi-Hamza <hamza.nawabi119@gmail.com>
2023-01-20 15:46:58 +0430

    all eamil section is ready

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 app.get('/', (req, res) => {
     console.log("Just got a request!")
-    res.send('Welcome To This Project')
+    res.send('Welcome To This Project Email Post Section Added')
 })
 
 app.post("/",(req,res)=>{
